cc3ea9594060baee0b163f73a20d928f0561a0a2
Yves Engetschwiler <info@bee-interactive.ch>
2025-04-02 19:45:36 +0200

    Fixed middleware incorrect definition

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append([
-            LocaleMiddleware::class,
+        $middleware->web(append: [
+            LocaleMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
