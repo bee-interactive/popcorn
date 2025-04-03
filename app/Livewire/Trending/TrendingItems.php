c7d3f02969a63b4590cfa01d5dbb83da5463f55f
Yves Engetschwiler <info@bee-interactive.ch>
2025-04-03 14:22:25 +0200

    Added trending items

--- app/Livewire/Trending/TrendingItems.php
+++ app/Livewire/Trending/TrendingItems.php
@@ -3,11 +3,30 @@
 namespace App\Livewire\Trending;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Cache;
+use App\Http\Integrations\Tmdb\TmdbConnector;
+use App\Http\Integrations\Tmdb\Requests\TrendingRequest;
 
 class TrendingItems extends Component
 {
-    public function render()
+    public array $results = [];
+
+    public function mount(TmdbConnector $connector)
     {
-        return view('livewire.trending.trending-items');
+        $this->results = Cache::remember('trending_items.' . auth()->user()->username, 7200, function () use ($connector) {
+            $results = [];
+
+            for ($i = 1; $i <= 6; $i++) {
+                $page = $connector->send(new TrendingRequest($i));
+
+                if ($page->failed()) {
+                    return [];
+                }
+
+                $results = array_merge($results, $page->json('results'));
+            }
+
+            return $results;
+        });
     }
 }
--- resources/views/livewire/trending/trending-items.blade.php
+++ resources/views/livewire/trending/trending-items.blade.php
@@ -2,11 +2,19 @@
     <flux:heading size="xl" level="1">{{ __('Trending') }}</flux:heading>
     <flux:text class="mt-2">{{ __('Discover items that are currently trending') }}</flux:text>
 
-    <div class="mt-12 max-w-md">
-        <flux:callout>
-            <flux:callout.heading icon="newspaper">{{ __('Part in development') }}</flux:callout.heading>
+    <div class="mt-12">
+        <flux:separator text="{{ __('Elements') }}" />
 
-            <flux:callout.text>{{ __('This feature is currently in development. Please come back later.') }}</flux:callout.text>
-        </flux:callout>
+        <div class="grid grid-cols-2 gap-2 gap-y-6 lg:gap-y-4 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 lg:gap-4 pt-4">
+            @foreach($results as $result)
+                <div class="relative">
+                    @if($result['poster_path'])
+                        <img class="shadow-lg rounded w-full h-full" src="https://image.tmdb.org/t/p/w400{{ $result['poster_path'] }}" alt="">
+                    @else
+                        <img class="shadow-lg rounded w-full h-full" src="{{ asset('img/placeholder.jpg') }}" alt="">
+                    @endif
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
